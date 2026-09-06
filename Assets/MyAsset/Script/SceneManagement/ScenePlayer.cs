9ec22b99ac2e81bc53e707b4497a68482338b42b
nemuineru <suteado16x16@gmail.com>
2026-09-07 00:41:11 +0900

    Aight, who thought what will happen when I adding transit?

--- Assets/MyAsset/Script/SceneManagement/ScenePlayer.cs
+++ Assets/MyAsset/Script/SceneManagement/ScenePlayer.cs
@@ -2,22 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TransitionsPlus;
 
 public class ScenePlayer : MonoBehaviour
 {
-    
+    [SerializeField]
+    TransitionAnimator transitionsPlus;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    float playTime = 0f;
+
     // Update is called once per frame
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            SceneManager.LoadScene("MainGameScene");
+            transitionsPlus.Play();
         }    
+        if( playTime >= transitionsPlus.profile.duration){
+            SceneManager.LoadScene("MainGameScene");
+        }
+        playTime += Time.deltaTime * (transitionsPlus.isPlaying ? 1 : 0);
     }
 }
